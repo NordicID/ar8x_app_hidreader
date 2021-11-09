da3ea9fb9e244f6e8b04e6080d331aa1787a13b7
ekkilm <ekkilm.ts@gmail.com>
2021-11-09 16:45:56 +0200

    Revert "Update v1.0.2"
    This reverts commit b8f029cca90bf2d4de1fc7ae943db53cb8aefe4b.

--- src/hidreader/hidreader.java
+++ src/hidreader/hidreader.java
@@ -1,10 +1,7 @@
 package hidreader;
 
 import java.net.MalformedURLException;
-import java.net.NetworkInterface;
 import java.net.Socket;
-import java.net.Inet4Address;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.nio.file.Files;
@@ -19,7 +16,7 @@ import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Enumeration;
+
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -74,7 +71,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.jar.Attributes.Name;
 import java.util.Iterator;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -300,17 +296,7 @@ public class hidreader {
 			}
 			catch(Exception e)
 			{
-				return null;
-			}
-		}
-		else if (replStr.startsWith("BARCODE")) {
-			try
-			{
-				return new EPCTagEngine(t.getEpcString()).buildBarcode(false, false, false);
-			}
-			catch(Exception e)
-			{
-				return null;
+				return t.getEpcString();
 			}
 		}
 		return null;
@@ -710,30 +696,6 @@ public class hidreader {
 		} catch (Exception ex)
 		{ }
 	}
-
-	// Get device interface IP address
-	private static String getInterfaceIpAddr(String interfaceName)
-	{
-        try{
-            String ip="";
-		    NetworkInterface networkInterface = NetworkInterface.getByName(interfaceName);
-            Enumeration<InetAddress> inetAddress = networkInterface.getInetAddresses();
-            InetAddress getIpAddress = inetAddress.nextElement();
-            
-			while(inetAddress.hasMoreElements()){
-                getIpAddress = inetAddress.nextElement();
-                if(getIpAddress instanceof Inet4Address && !getIpAddress.isLoopbackAddress()){
-                    ip = getIpAddress.getHostAddress();
-                    break;
-                }
-            }
-            return ip;
-        } 
-        catch (Exception E) {
-            System.err.println("System IP Exp: "+E.getMessage());
-            return null;
-        }
-    }
     
 	public static void main(String[] args) {
 		
@@ -813,8 +775,6 @@ public class hidreader {
 			public void connectedEvent() {
 				try {
 					mApi.setSetupTxLevel((int)txLevel);
-					String macAddr = mApi.getEthConfig().mac;
-					log("MAC address: " + macAddr);
 				} catch (Exception ex)
 				{ }
 			}
@@ -881,9 +841,7 @@ public class hidreader {
 
 			if (!mApi.isConnected())
 			{
-				// Get device Eth0 IP address
-				String ipAddr = getInterfaceIpAddr("eth0");
-				if (!connectNurIP(ipAddr, 4333))		
+				if (!connectNurIP("localhost", 4333))		
 				{					
 					publishStatus("noconn");
 					continue;
